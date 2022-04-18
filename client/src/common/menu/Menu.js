b769178d6c0a11ab2772d0c1ce67cdbd645ac2c6
Takuyay <mechanicalgiraffe@gmail.com>
2022-04-18 15:58:04 +0900

    Fix nav menu

--- client/src/common/menu/Menu.js
+++ client/src/common/menu/Menu.js
@@ -9,10 +9,18 @@ function Menu() {
   return (
     <nav id="menu">
       <Link to="/">Home</Link>
-      <Link to="new-thread">新規スレッド作成</Link>
-      <Link to="login">ログイン</Link>
-      <Link to="register">新規登録</Link>
-      <Link to="logout">ログアウト</Link>
+      {user.id ? (
+        <>
+          <Link to="new-thread">新規スレッド作成</Link>{" "}
+          <Link to="logout">ログアウト</Link>
+        </>
+      ) : (
+        <>
+          <Link to="login">ログイン</Link>
+          <Link to="register">新規登録</Link>
+        </>
+      )}
+
       {user.id ? user.username : "未ログイン"}
     </nav>
   );
