37fc3c45cb1a5f9e023ed92a05d53647ec4d50c4
Takuyay <mechanicalgiraffe@gmail.com>
2022-04-21 16:04:38 +0900

    Add hamburger menu

--- client/src/common/menu/Menu.js
+++ client/src/common/menu/Menu.js
@@ -1,29 +1,48 @@
-import "./Menu.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../features/auth/authSlice.js";
-import { BiUserCircle} from 'react-icons/bi';
+import { BiUserCircle } from "react-icons/bi";
+import { HiOutlineMenuAlt2 } from "react-icons/hi";
 
 function Menu() {
   const user = useSelector(selectUser);
+  const [showMenu, setShowMenu] = useState("");
+
+  const handleShowMenu = () => {
+    if (showMenu === "on") {
+      setShowMenu("");
+    } else {
+      setShowMenu("on");
+    }
+  };
 
   return (
-    <nav id="menu">
-      <Link to="/">Home</Link>
-      {user.id ? (
-        <>
-          <Link to="new-thread">新規スレッド作成</Link>{" "}
-          <Link to="logout">ログアウト</Link>
-        </>
-      ) : (
-        <>
-          <Link to="login">ログイン</Link>
-          <Link to="register">ユーザー登録</Link>
-        </>
-      )}
+    <>
+      <HiOutlineMenuAlt2 id="ham-btn" onClick={handleShowMenu} />
+      <nav id="menu" className={showMenu}>
+        <Link to="/">Home</Link>
+        {user.id ? (
+          <>
+            <Link to="new-thread">新規スレッド作成</Link>{" "}
+            <Link to="logout">ログアウト</Link>
+          </>
+        ) : (
+          <>
+            <Link to="login">ログイン</Link>
+            <Link to="register">ユーザー登録</Link>
+          </>
+        )}
 
-      {user.id ? <Link to="profile"><BiUserCircle /> {user.username}</Link> : "未ログイン"}
-    </nav>
+        {user.id ? (
+          <Link to="profile">
+            <BiUserCircle /> {user.username}
+          </Link>
+        ) : (
+          "未ログイン"
+        )}
+      </nav>
+    </>
   );
 }
 
