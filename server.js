339ac8b2d054371548c7f124569297055c8ed115
Takuyay <mechanicalgiraffe@gmail.com>
2022-05-09 13:06:31 +0900

    Fix port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app.js");
-const { port } = require("./src/configs/config.js");
+const { port } = require("./src/configs/config.js") || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
