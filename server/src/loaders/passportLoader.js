a1374b8cd3886025e6d27f387d1a81a8215c3f8a
Takuyay <mechanicalgiraffe@gmail.com>
2022-05-09 18:18:43 +0900

    Restructure directory

--- server/src/loaders/passportLoader.js
+++ server/src/loaders/passportLoader.js
@@ -12,7 +12,6 @@ module.exports = (app) => {
       secret: secret,
       resave: false,
       saveUninitialized: true,
-
       cookie: {
         secure: false,
         maxAge: 24 * 60 * 60 * 1000,
