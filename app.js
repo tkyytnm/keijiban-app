420887bc0c4db1360c0e0005a0361dc9622b5fcf
Takuyay <mechanicalgiraffe@gmail.com>
2022-05-09 19:31:42 +0900

    Move catchall handler to app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const loaders = require("./src/loaders");
+const path = require("path");
 
 loaders(app);
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 module.exports = app;
--- src/loaders/index.js
+++ src/loaders/index.js
@@ -2,17 +2,12 @@ const expressLoader = require("./expressLoader");
 const passportLoader = require("./passportLoader.js");
 const routesLoader = require("./routesLoader.js");
 const swaggerLoader = require("./swaggerLoader.js");
-const path = require("path");
 
 module.exports = (app) => {
   expressLoader(app);
   passportLoader(app);
   routesLoader(app);
   swaggerLoader(app);
-
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
 
   app.use((err, req, res, next) => {
     console.error(err.stack);
